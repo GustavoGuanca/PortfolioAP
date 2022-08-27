15bb188fd71b0de2220461d93afe326407119d17
Rodolfo Gustavo Guanca <93723007+GustavoGuanca@users.noreply.github.com>
2022-08-27 00:15:07 -0300

    Ultimo comiit

---
+++ BackEnd/src/main/java/com/portfolioAP/BackEnd/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolioAP.BackEnd.Repository;
+
+import com.portfolioAP.BackEnd.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
